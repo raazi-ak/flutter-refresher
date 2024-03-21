59225dc2fd859a3db1a71f6abf310e738ed9f455
Raazi Faisal <raazi.faisal@gmail.com>
2024-03-22 00:46:40 +0530

    implementing diet recommendations

--- lib/pages/CategoryList.dart
+++ lib/pages/CategoryList.dart
@@ -2,53 +2,103 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Container CategoryItem(Color contColor, String cateogoryName, String svgUrl) {
-  return Container(
-      margin: EdgeInsets.only(top: 20, bottom: 20),
-      padding: EdgeInsets.all(15),
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-          color: contColor,
-          boxShadow: [
-            BoxShadow(
-                blurRadius: 10,
-                spreadRadius: -0.5,
-                color: Colors.black.withOpacity(0.1))
-          ],
-          borderRadius: BorderRadius.circular(20)),
-      height: 130,
-      width: 100,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: SvgPicture.network(
-                svgUrl,
-                placeholderBuilder: (BuildContext context) => Container(
-                  padding: const EdgeInsets.all(20.0),
-                  child: const CircularProgressIndicator(),
+ListView CategoryItem(List<CategoryModel> category) {
+  return ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: category.length,
+    itemBuilder: (context, index) => Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 20),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: category[index].contColor,
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 10,
+                  spreadRadius: -0.5,
+                  color: Colors.black.withOpacity(0.1))
+            ],
+            borderRadius: BorderRadius.circular(20)),
+        height: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                width: 100,
+                height: 200,
+                child: SvgPicture.network(
+                  category[index].svgUrl,
+                  placeholderBuilder: (BuildContext context) => Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: const CircularProgressIndicator(),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            cateogoryName,
-            style: GoogleFonts.poppins(
-                textStyle:
-                    TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500)),
-          )
-        ],
-      ));
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              category[index].categoryName,
+              style: GoogleFonts.poppins(
+                  textStyle:
+                      TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500)),
+            )
+          ],
+        )),
+  );
 }
 
-class Category {
-  Category(
+ListView DietItem(List<DietModel> diet) {
+  return ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: diet.length,
+    itemBuilder: (context, index) => Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, right: 20),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: diet[index].contColor,
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 10,
+                  spreadRadius: -0.5,
+                  color: Colors.black.withOpacity(0.1))
+            ],
+            borderRadius: BorderRadius.circular(20)),
+        height: 150,
+        width: 100,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                width: 50,
+                child: SvgPicture.network(
+                  diet[index].svgUrl,
+                  placeholderBuilder: (BuildContext context) => Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: const CircularProgressIndicator(),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              diet[index].dietName,
+              style: GoogleFonts.poppins(
+                  textStyle:
+                      TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500)),
+            )
+          ],
+        )),
+  );
+}
+
+class CategoryModel {
+  CategoryModel(
       {required this.categoryName,
       required this.svgUrl,
       required this.contColor});
@@ -58,5 +108,14 @@ class Category {
   final String categoryName;
 }
 
-
-final List<Category>
+class DietModel {
+  DietModel(
+      {required this.dietDescription,
+      required this.dietName,
+      required this.contColor,
+      required this.svgUrl});
+  final String dietName;
+  final String dietDescription;
+  final Color contColor;
+  final String svgUrl;
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:refresher/pages/CategoryList.dart';
+import 'package:refresher/pages/categoryList.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final List<DietModel> dummyDiet = [
+    DietModel(dietName: 'Cereal Bowl', dietDescription: 'Easy|5 min|384 cal', svgUrl: "https://www.svgrepo.com/show/269879/cereal-wheat.svg", contColor: ),
+    DietModel(
+        dietName: 'Honey Pancakes',
+        dietDescription: 'Easy|30 minutes|409 cal '),
+    DietModel(dietName: 'Russian Salad', dietDescription: 'Easy|20 minutes|273 cal')
+  ];
+  final List<CategoryModel> dummyCategory = [
+    CategoryModel(
+        categoryName: "Muffins",
+        svgUrl: "https://www.svgrepo.com/show/152178/muffin.svg",
+        contColor: Color.fromRGBO(255, 204, 204, 1)), // Pastel pink
+    CategoryModel(
+        categoryName: "Pancakes",
+        svgUrl: "https://www.svgrepo.com/show/402294/pancakes.svg",
+        contColor: Color.fromRGBO(204, 255, 204, 1)), // Pastel green
+    CategoryModel(
+        categoryName: "Sandwiches",
+        svgUrl: "https://www.svgrepo.com/show/407373/sandwich.svg",
+        contColor: Color.fromRGBO(204, 204, 255, 1)), // Pastel blue
+    CategoryModel(
+        categoryName: "Bacon",
+        svgUrl: "https://www.svgrepo.com/show/268793/breakfast-bacon.svg",
+        contColor: Color.fromRGBO(255, 255, 204, 1)), // Pastel yellow
+    CategoryModel(
+        categoryName: "Eggs",
+        svgUrl: "https://www.svgrepo.com/show/228585/eggs-egg.svg",
+        contColor: Color.fromRGBO(255, 204, 255, 1)), // Pastel purple
+    CategoryModel(
+        categoryName: "Salads",
+        svgUrl: "https://www.svgrepo.com/show/235674/fruits-banana.svg",
+        contColor: Color.fromRGBO(204, 255, 255, 1)), // Pastel cyan
+    CategoryModel(
+        categoryName: "Smoothies",
+        svgUrl: "https://www.svgrepo.com/show/83163/juice.svg",
+        contColor: Color.fromRGBO(255, 255, 204, 1)), // Pastel yellow
+    CategoryModel(
+        categoryName: "Pizza",
+        svgUrl: "https://www.svgrepo.com/show/484414/pizza.svg",
+        contColor: Color.fromRGBO(255, 204, 204, 1)), // Pastel pink
+    CategoryModel(
+        categoryName: "Burgers",
+        svgUrl: "https://www.svgrepo.com/show/356623/sandwich-burger.svg",
+        contColor: Color.fromRGBO(204, 255, 204, 1)), // Pastel green
+    CategoryModel(
+        categoryName: "Sushi",
+        svgUrl: "https://www.svgrepo.com/show/505198/sushi.svg",
+        contColor: Color.fromRGBO(204, 204, 255, 1)), // Pastel blue
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +75,39 @@ class HomeView extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(left: 25.0),
             child: Category(),
+          ),
+          SizedBox(height: 150, child: CategoryItem(dummyCategory)),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Diet Recommendations",
+                  style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  )),
+                ),
+                Text(
+                  "For You",
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                        color: Colors.black45,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 300,
+                  child: DietItem(die),
+                )
+              ],
+            ),
           )
         ],
       ),
@@ -29,7 +117,6 @@ class HomeView extends StatelessWidget {
   Column Category() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
       children: [
         Text(
           "Category",
@@ -39,8 +126,48 @@ class HomeView extends StatelessWidget {
                   color: Colors.black,
                   fontWeight: FontWeight.w600)),
         ),
-        CategoryItem(Color.fromRGBO(168, 252, 172, 1), "Muffins",
-            "https://www.svgrepo.com/show/152178/muffin.svg")
+        // Container(
+        //     margin: EdgeInsets.only(top: 20, bottom: 20),
+        //     padding: EdgeInsets.all(15),
+        //     clipBehavior: Clip.hardEdge,
+        //     decoration: BoxDecoration(
+        //         color: category[0].contColor,
+        //         boxShadow: [
+        //           BoxShadow(
+        //               blurRadius: 10,
+        //               spreadRadius: -0.5,
+        //               color: Colors.black.withOpacity(0.1))
+        //         ],
+        //         borderRadius: BorderRadius.circular(20)),
+        //     height: 130,
+        //     width: 100,
+        //     child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //       crossAxisAlignment: CrossAxisAlignment.center,
+        //       children: [
+        //         Expanded(
+        //           child: Padding(
+        //             padding: const EdgeInsets.all(4.0),
+        //             child: SvgPicture.network(
+        //               category[0].svgUrl,
+        //               placeholderBuilder: (BuildContext context) => Container(
+        //                 padding: const EdgeInsets.all(20.0),
+        //                 child: const CircularProgressIndicator(),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //         const SizedBox(
+        //           height: 10,
+        //         ),
+        //         Text(
+        //           category[0].categoryName,
+        //           style: GoogleFonts.poppins(
+        //               textStyle: TextStyle(
+        //                   fontSize: 14.0, fontWeight: FontWeight.w500)),
+        //         )
+        //       ],
+        //     ))
       ],
     );
   }
@@ -95,7 +222,6 @@ class HomeView extends StatelessWidget {
                         child: VerticalDivider(
                           color: Colors.black.withOpacity(0.15),
                           thickness: 1.3,
-                          key: key,
                         ),
                       ),
                     ),
